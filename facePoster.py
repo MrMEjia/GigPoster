0c7709a679fd2ded34c0bb50b5b15387cfc899ac
AWS <benjaminbyron@gmail.com>
2017-06-11 23:27:39 +0000

    Added functionality for separating key into config file.

--- facePoster.py
+++ facePoster.py
@@ -1,10 +1,10 @@
 import facebook
-
+from config import key     
 
 # this is working and the token should be infinite
 
 def main(msg):
     graph = facebook.GraphAPI(
-        "EAAYZBui8MtxsBAJi3hO29GKwHJHITOmstUHoyXquKsSH0RGGKkbwSxSWPESo4iaSEpLnVR9iVks8P0lFrm2ahlkn4QhgbicC3FZC8tK9wT0XEn4sF6VeRKl3XA3KLYwy6fGfPNXwIeQ9aoUtBezK3IHbpDxKcZD",
+        key ,
         version='2.7')
     graph.put_object("206114322854301", "feed", message=msg)
