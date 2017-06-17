b48931e0d41ef74615477ff3ae729f60555f2293
BMejia <Benjaminbyron@gmail.com>
2017-06-17 00:23:55 -0400

    Forgot to add GigSplit

---
+++ GigSplit.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 23:17:55 2017
+
+@author: benny
+"""
+
+
+
+
+def main(gig):
+    nums = "01234567890-"
+    parsed = []
+
+    for g in gig:
+        name = ""
+        time = ""
+        for x in g:
+            if x not in nums:
+                name += x
+            if x in nums:
+                time += x
+                
+        phrase = '"' + name + "from " + time + '"'
+        parsed.append(phrase)
+    
+    if len(parsed) < 2:
+        total = parsed[0]
+    else:        
+        total =  parsed[0] + " and " + parsed[1]
+            
+    
+    return(total)
